615176d6c19c732d18da4149e15f8f0d6ecebcd6
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-08-22 16:39:53 -0500

    Add files via upload

---
+++ Introduccion Listas/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(2);
+
+
+            Console.WriteLine(numbers.Count);
+
+            List<int> numbers2 = new List<int>()
+            {
+               1,6,7,10,14
+            };
+            Console.WriteLine(numbers2.Count);
+            numbers2.Add(55);
+            Console.WriteLine(numbers2.Count);
+
+            numbers2.Clear();
+                Console.WriteLine(numbers2.Count);
+
+
+            List<string> countries = new List<string>()
+            {
+                "Mexico", "Colombia", "Argentina"
+            };
+            Console.WriteLine(countries.Count);
+        }
+    }
+}
