d54b3d601a5e404ce601f629d91e57a2890c575b
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-08-22 14:15:10 -0500

    Add files via upload

---
+++ SentenciDoWhile/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int k = 0;
+
+while(k < 10)
+{
+    Console.WriteLine("Iteracion de k "+k);
+    k++;
+};
+
+int j = 0;
+while (j < 100)
+{
+    if (j > 2)
+        break;
+
+
+    Console.WriteLine("Iteracaion de J " + j);
+    j++;
+}
+
+
+string[] friends = new string[7]{
+    "pacho",
+    "paco",
+    "ana",
+    "ruben",
+    "karla",
+    "luis",
+    "hector"
+};
+
+int index = 0;
+while (index < friends.Length)
+{
+    Console.WriteLine(friends[index]);
+    index++;
+}
+
+
+bool run = false;
+do
+{
+    Console.WriteLine("Entra una vez y ya");
+} while (run);
+
+
+static void show()
+{
+    Console.WriteLine("Entro un vez y ya");
+}
---
+++ SentenciaSwitch/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+int op = 7;
+
+switch (op)
+{
+    case 1:
+        Console.WriteLine("selecccionaste el 1");
+        break;
+
+    case 2:
+        Console.WriteLine("Seleccionaste el 2");
+        break;
+
+    case 3:
+        Console.WriteLine("Seleccionaste la opcion 3");
+        break;
+
+    case 4:
+        Console.WriteLine("Seleccionaste la opcion 4");
+        break;
+
+    case < 0:
+        Console.WriteLine("Fuera de rango ");
+        break;
+
+    case > 100:
+        Console.WriteLine("Muy fuera de rango");
+        break;
+
+    case > 4 and < 10:
+        Console.WriteLine("Seleccionaste una opcion entre 4 y 10");
+        break;
+
+    default:
+        Console.WriteLine("Invalido");
+        break;
+}
+
+//Evaluar condiciones con switch case 
