c5b4f22228f8b012e96b06e176e3d4438fdc1b05
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-08-22 14:15:43 -0500

    Add files via upload

---
+++ Static clasa de objetos/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more informatio111n
+
+class Program {
+    static void Main(string[] args) { 
+
+    People people1 = new People()
+    {
+        Name = "ytrewq",
+        Age = 43
+    };
+
+
+        People people2 = new People()
+        {
+            Name = "qwerty",
+            Age = 34
+        };
+
+        Console.WriteLine(People.GetCount());
+    }
+
+    public class People
+    {
+        public static int Count = 0;
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public People()
+        {
+            Count++;
+        }
+
+        public static string GetCount()
+        {
+            return $"Esta clase se ha utilizado {Count} veces";
+        }
+    }
+}
---
+++ TiposDeDatos/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double doubleNumber = 12.2165; //double para decimales o enteros
+char character = 'a'; //Si es una letra o numero solo se ponen comillas simples ''
+string name = "Zaiko"; // si es mas de un caracter comillas dobles " "
+bool isDead = false; //Se puede guardar un true o false
+
+Console.WriteLine(isDead);
