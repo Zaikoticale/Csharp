6af9c9beee4016ab4c56dea79770a11851b95d8a
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-08-22 16:31:42 -0500

    Add files via upload

---
+++ Serializacion de objetos JSON/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.Json;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beer myBeer = new Beer()
+            {
+                Name = "Poker",
+                Brand = "Bavaria"
+            };
+            // string json = "{\"Name\":\"Poker\",\"Brand\":\"Bavaria\"}";
+            string json = JsonSerializer.Serialize(myBeer);
+            Beer beer = JsonSerializer.Deserialize<Beer>(json);
+
+            Beer[] beers = new Beer[]
+            {
+                new Beer()
+                {
+                Name = "Poker",
+                Brand = "Bavaria"
+                },
+                new Beer()
+                {
+                    Name="Aguila",
+                    Brand="Marca"
+                }
+            };
+
+            //    string json2 = "[" +
+            //        "{\"Name\":\"Poker\",\"Brand\":\"Bavaria\"}," +
+            //        "{\"Name\":\"Aguila\",\"Brand\":\"Marca\"}" +
+            //        "]";
+
+            string json2 = JsonSerializer.Serialize(beers);
+            Beer[] beers2 = JsonSerializer.Deserialize<Beer[]>(json2);
+
+        }
+
+        public class Beer
+        {
+            public string Name { get; set; }
+            public string Brand { get; set; }
+        }
+    }
+}
