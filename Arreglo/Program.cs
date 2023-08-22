fb3d599be32ee457c631294fbcad5d51617e3b96
Alejandro Vega <107321437+Zaikoticale@users.noreply.github.com>
2023-08-22 14:14:12 -0500

    Add files via upload

---
+++ Arreglo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] friends = new string[8]{
+    "pacho",
+    "paco",
+    "ana",
+    "ruben",
+    "karla",
+    "luis",
+    null,
+    null
+};
+
+friends[0] = "hector";
+friends[6] = "carlos";
+
+
+Console.WriteLine(friends[0]);
+Console.WriteLine(friends[1]);
+Console.WriteLine(friends[2]);
+Console.WriteLine(friends[3]);
+Console.WriteLine(friends[4]);
+Console.WriteLine(friends[5]);
+Console.WriteLine(friends[6]);
